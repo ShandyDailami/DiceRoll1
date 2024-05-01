d6cef3b3045299fed4c3074479005b04c0d54215
Shandy Dailami <shandydailami@gmail.com>
2024-05-01 09:41:47 +0800

    Clean Project

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         // Calls the button id
         val rollBtn: Button = findViewById(R.id.btn_roll)
 
+        // Calls the image view id
         val diceImgFirst: ImageView = findViewById(R.id.iv_img_first)
         val diceImgSecond: ImageView = findViewById(R.id.iv_img_second)
 
